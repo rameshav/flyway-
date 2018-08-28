0fa432247cd4d281fa302e428d6e24a91e37296f
Ubuntu <ubuntu@ip-172-31-37-124.us-east-2.compute.internal>
2018-08-28 11:58:55 +0000

    flyway src

---
+++ src/main/resources/db/migration/V3__Create_person_table.sql
@@ -0,0 +1,5 @@
+insert into PERSON (ID, NAME) values (1, 'Axel');
+insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
+insert into PERSON (ID, NAME) values (4, 'Ms. Tar');
+insert into PERSON (ID, NAME) values (5, 'Mr. James');
